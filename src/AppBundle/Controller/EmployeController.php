c3e0bda068634de1bd7ceb9ed99d444d476bba78
Talim <talimidiusnx@gmail.com>
2016-12-08 09:50:55 +0100

    Màj "Employe"

--- src/AppBundle/Controller/EmployeController.php
+++ src/AppBundle/Controller/EmployeController.php
@@ -5,16 +5,22 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/gestemp")
+ */
 class EmployeController extends Controller
 {
     /**
-     * @Route("/gestemp/voir/{id}")
+     * @Route(
+     *     path="/employe/{id}",
+     *     name="employer_voir",
+     *     requirements={"id"="\d+"}
+     *
+     * )
      */
     public function voirAction($id)
     {
-        return $this->render('AppBundle:Employe:voir.html.twig', array(
-            // ...
-        ));
+        return $this->render('AppBundle:Employe:voir.html.twig', array("id" => $id));
     }
 
 }
